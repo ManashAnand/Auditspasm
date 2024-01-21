4a348ddc0207e8f08de7d97552d149ed83cf00ca
MANASH <anandmanash321@gmail.com>
2024-01-22 00:04:32 +0530

    updated form

--- app/Audit/page.jsx
+++ app/Audit/page.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import AuditCard from '../component/AuditCard'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/navigation'
 
 const Audit = () => {
+
+
+
+  const state = useSelector(state => state.user)
+  console.log(state)
+  
+  const router = useRouter()
+
+  useEffect(() => {
+    if(Object.keys(state).length == 0 ){
+      router.push("/")
+    } 
+  },[])
+  
   return (
     <>
     <section className="px-10  py-10 sm:px-20 text-gray-600 body-font bg-[#fefae0]  sm:grid grid-cols-3 grid-rows-2 gap-2 flex flex-col">
-        <AuditCard/>
-        <AuditCard/>
-        <AuditCard/>
-        <AuditCard/>
-        <AuditCard/>
-        <AuditCard/>
+        <AuditCard formUrl={"https://forms.gle/6GENJDVFqczJyLocA"}/>
+        <AuditCard formUrl={"https://forms.gle/6GENJDVFqczJyLocA"}/>
+        <AuditCard formUrl={"https://forms.gle/6GENJDVFqczJyLocA"}/>
+        <AuditCard formUrl={"https://forms.gle/6GENJDVFqczJyLocA"}/>
+        <AuditCard formUrl={"https://forms.gle/6GENJDVFqczJyLocA"}/>
+        <AuditCard formUrl={"https://forms.gle/6GENJDVFqczJyLocA"}/>
       </section>
     </>
   )
--- app/component/AuditCard.jsx
+++ app/component/AuditCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import iqac from '../assets/iqac2.png'
 import Image from 'next/image'
-const AuditCard = () => {
+import Link from 'next/link'
+const AuditCard = ({formUrl}) => {
   return (
     <>
     <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
@@ -38,11 +39,11 @@ const AuditCard = () => {
    
   </div>
   <div class="p-6 pt-3">
-    <button
+    <Link href={formUrl}
       class="block w-full select-none rounded-lg bg-gray-900 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       type="button">
       Go to form
-    </button>
+    </Link>
   </div>
 </div>  
     </>
